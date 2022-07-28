c78a5ef88b02f371e4b8b8e3ce3b06ea5f8f7650
kai <kai.gao@feibit.com>
2022-07-28 14:57:09 +0800

    更新初始化

---
+++ kkmall-common/src/test/java/com/kyson/mall/common/KkmallCommonApplicationTests.java
@@ -0,0 +1,14 @@
+package com.kyson.mall.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkmallCommonApplicationTests {
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
