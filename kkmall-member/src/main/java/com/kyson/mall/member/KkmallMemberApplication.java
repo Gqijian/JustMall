9704fecd436470f0a3dd7cc6ac8deebcb1514ae1
kai <kai.gao@feibit.com>
2022-08-01 17:17:08 +0800

    add alibaba nacos

--- kkmall-member/src/main/java/com/kyson/mall/member/KkmallMemberApplication.java
+++ kkmall-member/src/main/java/com/kyson/mall/member/KkmallMemberApplication.java
@@ -2,7 +2,9 @@ package com.kyson.mall.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class KkmallMemberApplication {
 
